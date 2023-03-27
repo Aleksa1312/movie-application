d9a4782b418c284436892c1f685603f08cbecc68
aleksa <asesa1312@gmail.com>
2023-03-27 19:02:19 +0200

    initial commit - added tailwindcss

---
+++ types/next.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_SUPABASE_ANON: string;
+        NEXT_PUBLIC_SUPABASE_URL: string;
+        NEXT_PUBLIC_TMDB_KEY: string;
+    }
+}
